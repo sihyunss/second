a67086966caf84e26857650d4d5772c13bf915fd
sihyunss <kimseehuen8953@naver.com>
2024-08-10 11:49:38 +0900

    lottery

--- library_random.py
+++ library_random.py
@@ -3,6 +3,7 @@
 import random
 import time
 
+#사용자에게 4개의 번호를 받는 함수
 def buy_lottery():
     user_lottery_number = []
     while True:
@@ -19,10 +20,40 @@ def buy_lottery():
             print('접수 완료되었습니다.')
             return user_lottery_number
 
-def check_lottery_number(user):
-    print(user)
+#추첨하고, random함수가 확률을 계산하기 위해서 배열을 만들어 나온 수를 더하고, 몇개 담청됐는지 확인하는 함수
+def check_lottery_number(user,ai):
+    num = 0
+    result_lottery_number = []
+    for i in range(4):
+        a = random.randint(1,10)
+        result_lottery_number.append(a)
+        ai[a-1]+=1
+        if user[i] == a:
+            num+=1
+    print("번호는 ",end = ' ')
+    for i in result_lottery_number:
+        print(i,end = ' ')
+    print('입니다')
+    print(str(num)+"개 담청되었습니다")
+
+    time.sleep(3)
+
+#배열에 갯수가 가장 높은 수를 추천해주는 함수
+def number_recommendation(ai):
+    ai_copy = ai[:]
+    recommend = []
+    for _ in range(4):
+        max_num = max(ai_copy)
+        if max_num == 0 :
+            break
+        num = ai_copy.index(max_num)+1
+        recommend.append(num)
+        ai_copy[num-1] = 0
+    print("추천번호 :",recommend)
+    time.sleep(1)
 
 user_lottery_numbers=[]
+probability_lottery_number = [0]*10
 
 while True:
     
@@ -34,4 +65,6 @@ while True:
         user_lottery_numbers = buy_lottery()
         print(user_lottery_numbers)
     elif num == 2:
-        check_lottery_number(user_lottery_numbers)
+        check_lottery_number(user_lottery_numbers,probability_lottery_number)
+    elif num == 3:
+        number_recommendation(probability_lottery_number)
